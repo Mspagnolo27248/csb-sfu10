57f20cf0f64f1bd5031e655ae1459d4bcbecacd2
Code Sandbox <hello@codesandbox.io>
2021-11-14 19:11:46 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b2a500a685902139dae0a77e5f844871.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eef2431332cfc3414dee2ec4f2ba5c",
+    "url": "https://mspagnolo27248.github.io/csb-sfu10/index.html"
+  },
+  {
+    "revision": "2fb00f18ef40988468a8",
+    "url": "https://mspagnolo27248.github.io/csb-sfu10/static/js/2.268861b2.chunk.js"
+  },
+  {
+    "revision": "cd36c199e40c3b38c3bf",
+    "url": "https://mspagnolo27248.github.io/csb-sfu10/static/js/main.a9ca1b21.chunk.js"
+  },
+  {
+    "revision": "a00532d992c23301eec3",
+    "url": "https://mspagnolo27248.github.io/csb-sfu10/static/js/runtime-main.0bdb8bd5.js"
+  }
+]);
